f3b8628ea7e68843a23943b11c2fda2bd809064b
naelafauzul <naelafauzulm@gmail.com>
2024-01-26 12:04:15 +0700

    Initial Commit

---
+++ OnboardingScreen/OnboardingScreenApp.swift
@@ -0,0 +1,17 @@
+//
+//  OnboardingScreenApp.swift
+//  OnboardingScreen
+//
+//  Created by Naela Fauzul Muna on 26/01/24.
+//
+
+import SwiftUI
+
+@main
+struct OnboardingScreenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
